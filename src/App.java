1d74ea5448f4167eebe5246a6d03a96804ece59e
romanlukichev <roman.lukichev@yahoo.com>
2018-01-26 14:01:53 -0800

    Factorial function with using int type

--- src/App.java
+++ src/App.java
@@ -1,27 +1,25 @@
+import java.math.BigInteger;
+
 public class App {
 
     public static void main(String[] args) {
-        String s1 = new String("Madam, I'm Adam!");
-        isPalindrome(s1);
+        System.out.println(factorial(8));
     }
 
-    public static boolean isPalindrome(String text) {
-        boolean isPalindrome = false;
-        String text2 = text.replaceAll(  "[^A-Za-z0-9]+" , "");
-        //System.out.println(text2);
-        String text3 = text2.toLowerCase();
-        //System.out.println(text3);
-        StringBuilder text4 = new StringBuilder(text3);
-        text4.reverse();
-        //"^[A-Z]"
-        //"^.*[A-Z]"
-        //"[A-Z]+"
-        //"[^A-Za-z0-9]+"
-        //System.out.println(text4);
-        if(text4.toString().equals(text3)){
-            isPalindrome = true;
+    /**
+     * Calculates factorial of given <code>value</code>.
+     *
+     * @param value positive number
+     * @return factorial of <code>value</code>
+     */
+    public static int factorial(int value) {
+
+
+        if(value==1){
+            return 1;
         }
-        return isPalindrome; // your implementation here
+        else
+        return factorial( value - 1 ) * value; // your implementation here
     }
 
 }
