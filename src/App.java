dba14d4897da7a3927c0565876d6adbaed06cae0
romanlukichev <roman.lukichev@yahoo.com>
2018-01-27 20:20:10 -0800

    Making list of quotes in a role play. version1.

--- src/App.java
+++ src/App.java
@@ -4,41 +4,45 @@ import java.util.Arrays;
 public class App {
 
     public static void main(String[] args) {
-        int[] array1 = { 1 , 3 , 5 , 5 , 6 };  //, 7 , 15
-        int[] array2 = { 0 , 3 , 5 , 8 , 10 , 15 , 18 , 20 , 25 , 30 };
 
-        int[] array3 = mergeArrays(array1,array2);
-        System.out.println(Arrays.toString(array1));
-        System.out.println(Arrays.toString(array2));
-        System.out.println(Arrays.toString(array3));
+        String[] roles = { "Городничий" , "Аммос Федорович" , "Артемий Филиппович" , "Лука Лукич" , "Роман" , "Лука"  };
+        String[] textLines = {
+                "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.'" ,
+                "Аммос Федорович: Как ревизор?" ,
+                "Артемий Филиппович: Как ревизор?" ,
+                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем." ,
+                "Аммос Федорович: Вот те на!" ,
+                "Лука: Мы угостим ревизора чаем!",
+                "Артемий Филиппович: Вот не было заботы, так подай!" ,
+                "Лука Лукич: Господи боже! еще и с секретным предписаньем!"
+        };
+
+        printWholeBook( roles , textLines );
     }
 
-    /**
-     * Merges two given sorted arrays into one
-     *
-     * @param m1 first sorted array
-     * @param m2 second sorted array
-     * @return new array containing all elements from m1 and m2, sorted
-     */
-    public static int[] mergeArrays(int[] m1, int[] m2) {
-        int[] m3 = new int[ m1.length + m2.length ];
-        int m3index = 0;
-        int m1index = 0;
-        int m2index = 0;
-        while( m1index < m1.length && m2index < m2.length){
-            if ( m1[m1index] <= m2[m2index] ){
-                m3[m3index++] = m1[m1index++];
-            } else {
-                m3[m3index++] = m2[m2index++];
+    public static void printWholeBook( String[] roles , String[] textLines ){
+        for (String role: roles ) {
+            //System.out.println("==============================================");
+            System.out.println( role + ":" );
+
+            for ( int i=0; i<textLines.length; i++){
+                if( role.equals( textLines[i].split(":")[0]) )
+                    System.out.println(
+                                      i+1 + ") "
+                                    + textLines[i].split(":")[1]
+                    );
+                // each Sring will be split into two-dimensional array by ":" character .
             }
+//                we could have used for each loop here
+//                for ( String textLine: textLines){
+//                if( role.equals( textLine.split(":")[0]) )
+//                    System.out.println( textLine.split(":")[1] );
+//                //Because each String will be split into two-dimensional array by ":" character .
+//            }
+
         }
-        while( m2index < m2.length){   // if m2 array is longer
-           m3[m3index++] = m2[m2index++];
-        }
-        while( m1index < m1.length){  // if m1 array is longer
-                m3[m3index++] = m1[m1index++];
-        }
-        return m3;
+
     }
+
 
 }
