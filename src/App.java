15e3d2571acbf8f7b452ff5c8ff3be55a0c9d878
romanlukichev <roman.lukichev@yahoo.com>
2018-01-28 16:53:30 -0800

    Making list of quotes in a role play. version2.

--- src/App.java
+++ src/App.java
@@ -7,7 +7,7 @@ public class App {
 
         String[] roles = { "Городничий" , "Аммос Федорович" , "Артемий Филиппович" , "Лука Лукич" , "Роман" , "Лука"  };
         String[] textLines = {
-                "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.'" ,
+                "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить: вам пренеприятное известие: к нам едет ревизор." ,
                 "Аммос Федорович: Как ревизор?" ,
                 "Артемий Филиппович: Как ревизор?" ,
                 "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем." ,
@@ -17,23 +17,32 @@ public class App {
                 "Лука Лукич: Господи боже! еще и с секретным предписаньем!"
         };
 
-        printWholeBook( roles , textLines );
+        System.out.println( printWholeBook( roles , textLines ) );
+
     }
 
-    public static void printWholeBook( String[] roles , String[] textLines ){
+    private static String printWholeBook( String[] roles , String[] textLines ){
+
+        String resultString ="";
         for (String role: roles ) {
-            //System.out.println("==============================================");
-            System.out.println( role + ":" );
+            System.out.println("==============================================");
+            System.out.println(role + ":");   // UNIX like end of string
 
             for ( int i=0; i<textLines.length; i++){
-                if( role.equals( textLines[i].split(":")[0]) )
+                if( role.equals(
+                               textLines[i].substring( 0, textLines[i].indexOf(":") )  // everything before ":"
+                               )
+                  ) {
+
                     System.out.println(
-                                      i+1 + ") "
-                                    + textLines[i].split(":")[1]
-                    );
-                // each Sring will be split into two-dimensional array by ":" character .
+                                         (i + 1) + ") " + textLines[i].substring(
+                                                                        textLines[i].indexOf(":")+1, textLines[i].length()
+                                                                                )
+                                      );
+                }
+                // each String will be split into two-dimensional array by ":" character .
             }
-//                we could have used for each loop here
+//                we could have used for each loop here  // this syntax looks easier.
 //                for ( String textLine: textLines){
 //                if( role.equals( textLine.split(":")[0]) )
 //                    System.out.println( textLine.split(":")[1] );
@@ -42,6 +51,7 @@ public class App {
 
         }
 
+        return resultString;
     }
 
 
